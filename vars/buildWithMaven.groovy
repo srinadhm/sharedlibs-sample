873978d264a2e9ddb36d3e00e537064c27111203
srinadhm <30432794+srinadhm@users.noreply.github.com>
2025-10-16 11:22:21 -0400

    Update buildWithMaven.groovy

--- vars/buildWithMaven.groovy
+++ vars/buildWithMaven.groovy
@@ -1,8 +1,9 @@
-def call() {
+def call(String goal) {
     //stage('Build with Maven') {
      //   echo "Running Maven Goals: ${goals}"
       //  sh "mvn -s ${libraryResource('config/maven-settings.xml')} ${goals}"
      // }
-    sh "mvn clean install"
+   // sh "mvn clean install"
+    sh "mvn clean ${goal}"
   
 }
