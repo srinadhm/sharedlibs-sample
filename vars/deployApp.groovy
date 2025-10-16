cb2679cd7717d141d2ff886e3ce2bf4f04994eae
srinadhm <30432794+srinadhm@users.noreply.github.com>
2025-10-16 11:28:46 -0400

    Create deployApp.groovy

---
+++ vars/deployApp.groovy
@@ -0,0 +1,9 @@
+def call(String destination = '/Users/mupparajusrinadh/Documents/Jenkins-Workspace', String artifactlocation = 'target/*.jar') {
+    stage('Deploy Application') {
+        echo "Deploying application to ${destination}”
+        sh """
+        cp ${artifactlocation} ${destination}
+      
+        """
+    }
+}
