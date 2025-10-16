14ada3a93e8a154cdc7d1f576a0e9475971c1449
srinadhm <30432794+srinadhm@users.noreply.github.com>
2025-10-16 11:34:07 -0400

    Updated deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,6 +1,6 @@
 def call(String destination = '/Users/mupparajusrinadh/Documents/Jenkins-Workspace', String artifactlocation = 'target/*.jar') {
     stage('Deploy Application') {
-        echo "Deploying application to ${destination}”
+        echo "Deploying application to ${destination}"
         sh """
         cp ${artifactlocation} ${destination}
       
