09d098d27f3eb585709dbd3929d6bb9ff1f2fed9
srinadhm <30432794+srinadhm@users.noreply.github.com>
2025-10-17 17:02:31 -0400

    Update resource path for Linux script loading

---
+++ vars/loadLinuxScript.groovy
@@ -0,0 +1,5 @@
+def call(Map config = [:]) { 
+  def scriptcontents = libraryResource "resources/com/jenkinsresource/scripts/linux/${config.name}"    
+  writeFile file: "${config.name}", text: scriptcontents 
+  sh "chmod a+x ./${config.name}"
+} 
