9680c32369763320aa4799a2dcb215a0c0e53909
srinadhm <30432794+srinadhm@users.noreply.github.com>
2025-10-17 20:56:00 -0400

    Fix libraryResource path in loadLinuxScript

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "resources/com/jenkinsresource/scripts/linux/${config.name}"    
+  def scriptcontents = libraryResource "com/jenkinsresource/scripts/linux/${config.name}"    
   writeFile file: "${config.name}", text: scriptcontents 
   sh "chmod a+x ./${config.name}"
 } 
